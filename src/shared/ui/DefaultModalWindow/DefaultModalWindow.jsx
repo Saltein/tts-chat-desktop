7388675f29ae234d6420ae2169cbc0e4d798411e
Никита <nikitval@list.ru>
2026-05-08 12:16:44 +0300

    fix: если зажал мышь внутри модалки, увёл курсор, отпустил на фоне - модалка закроется

--- src/shared/ui/DefaultModalWindow/DefaultModalWindow.jsx
+++ src/shared/ui/DefaultModalWindow/DefaultModalWindow.jsx
@@ -1,21 +1,32 @@
-import { DefaultWidgetShape } from '../../widgets/DefaultWidgetShape/DefaultWidgetShape'
-import s from './DefaultModalWindow.module.scss'
-import { createPortal } from 'react-dom'
+import { DefaultWidgetShape } from "../../widgets/DefaultWidgetShape/DefaultWidgetShape";
+import s from "./DefaultModalWindow.module.scss";
+import { createPortal } from "react-dom";
 
-export const DefaultModalWindow = ({ children, onClose, backgroundColor, padding, title }) => {
-    const handleClick = (e) => {
+export const DefaultModalWindow = ({
+    children,
+    onClose,
+    backgroundColor,
+    padding,
+    title,
+}) => {
+    const handleBackgroundMouseDown = (e) => {
         if (e.target === e.currentTarget) {
-            e.stopPropagation()
-            onClose()
+            onClose();
         }
-    }
+    };
 
     return createPortal(
-        <div className={s.background} onClick={handleClick}>
-            <DefaultWidgetShape animated backgroundColor={backgroundColor && backgroundColor} padding={padding && padding} title={title} justifyTitle={'center'}>
+        <div className={s.background} onMouseDown={handleBackgroundMouseDown}>
+            <DefaultWidgetShape
+                animated
+                backgroundColor={backgroundColor && backgroundColor}
+                padding={padding && padding}
+                title={title}
+                justifyTitle={"center"}
+            >
                 {children}
             </DefaultWidgetShape>
         </div>,
-        document.body
-    )
-}
+        document.body,
+    );
+};

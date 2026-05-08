6302ccd6bc5d2fe71672f7890a48dc40abf7dda5
Никита <nikitval@list.ru>
2026-05-08 13:47:05 +0300

    fix: Слишком длинные никнеймы сворачиваются с троеточием (раньше ломали лайаут)

--- src/shared/lib/getRandomInt.js
+++ src/shared/lib/getRandomInt.js
@@ -1,6 +1,6 @@
 export function getRandomInt(min, max) {
-  // Включая min и max
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+    // Включая min и max
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }

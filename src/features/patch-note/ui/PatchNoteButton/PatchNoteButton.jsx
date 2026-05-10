eb92019b01d9e8978ee89003a3a0789139be9624
Никита <nikitval@list.ru>
2026-05-10 10:22:55 +0300

    feat: ссылка на релизы

--- src/features/patch-note/ui/PatchNoteButton/PatchNoteButton.jsx
+++ src/features/patch-note/ui/PatchNoteButton/PatchNoteButton.jsx
@@ -5,6 +5,8 @@ import { useState } from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import markdown from "../../../../shared/assets/texts/updates.md?raw";
+import { DefaultDivider } from "../../../../shared/ui";
+import ExternalLinkIcon from "../../../../shared/assets/icons/external-link.svg?react";
 
 export const PatchNoteButton = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
@@ -27,6 +29,19 @@ export const PatchNoteButton = () => {
                         <ReactMarkdown remarkPlugins={[remarkGfm]}>
                             {markdown}
                         </ReactMarkdown>
+                        <DefaultDivider />
+                        <div
+                            className={s.link}
+                            onClick={() => {
+                                window.electronAPI?.openExternal(
+                                    "https://github.com/Saltein/tts-electron-version/releases",
+                                );
+                            }}
+                            title="https://github.com/Saltein/tts-electron-version/releases"
+                        >
+                            История изменений и прошлые версии
+                            <ExternalLinkIcon className={s.linkIcon} />
+                        </div>
                     </div>
                 </DefaultModalWindow>
             )}
